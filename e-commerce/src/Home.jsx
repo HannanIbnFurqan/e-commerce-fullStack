8eb5b089be188cf190c7ef24def04008193e74eb
HannanIbnFurqan <mohdhannan7913@gmail.com>
2025-01-11 19:14:27 +0530

    solve bug

--- e-commerce/src/Home.jsx
+++ e-commerce/src/Home.jsx
@@ -1,20 +1,21 @@
-import products from './data.js'
+import products from './data.js';
+
 let Home = () => {
     let first_obj = products[0];
     console.log(first_obj);
     return (
-        <div className="border-2 border-red-50 w-screen h-[87vh]">
-            <div className="card card-compact bg-base-100 w-96 shadow-xl">
+        <div className="border-2 border-white w-screen h-[87vh]">
+            <div className="card card-compact bg-white w-96 shadow-xl">
                 <figure>
                     <img
                         src={`${first_obj.thumbnail}`}
-                        alt="Shoes" />
+                        alt="Product Thumbnail" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">{first_obj.category}</h2>
-                    <p>{first_obj.description}</p>
+                    <h2 className="card-title text-black">{first_obj.category}</h2>
+                    <p className="text-black">{first_obj.title}</p>
                     <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary text-white">Buy Now</button>
                     </div>
                 </div>
             </div>
@@ -22,4 +23,4 @@ let Home = () => {
     )
 }
 
-export default Home
+export default Home;
--- e-commerce/src/Navbar.jsx
+++ e-commerce/src/Navbar.jsx
@@ -1,18 +1,18 @@
 let Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-slate-100">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Mood Mart</a>
+                <a className="btn btn-ghost text-xl text-black">Mood Mart</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Cart</a></li>
-                    <li><a>Profie</a></li>
-                    <li><a>Theme</a></li>
+                    <li><a className="text-black">Cart</a></li>
+                    <li><a className="text-black">Profile</a></li>
+                    <li><a className="text-black">Theme</a></li>
                 </ul>
             </div>
         </div>
     )
 }
 
-export default Navbar
+export default Navbar;
