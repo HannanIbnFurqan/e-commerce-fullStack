be83a71ffe6088d62520316f702087b297f570fb
Mohd Hannan <mohdhannan7913@gmail.com>
2025-01-16 23:42:42 +0530

    Add filters

--- e-commerce/src/Home.jsx
+++ e-commerce/src/Home.jsx
@@ -7,9 +7,20 @@ let Home = () => {
    
     return (
         <div className=" w-screen h-[87vh]">
-            <div>
-            <button className="btn btn-outline btn-primary">Primary</button>
-
+            <div className='flex justify-around mt-1.5'>
+               <button className="btn btn-outline btn-primary">Primary</button>
+               <button className="btn btn-outline btn-primary">Primary</button>
+               <button className="btn btn-outline btn-primary">Primary</button>
+               <div className='flex gap-2'>
+               <input
+                    type="text"
+                    placeholder="Type here"
+                    className="input input-bordered input-primary w-full max-w-xs" />
+                    <button className="btn btn-primary">Primary</button>
+               </div>
+               <button className="btn btn-outline btn-primary">Primary</button>
+               <button className="btn btn-outline btn-primary">Primary</button>
+               <button className="btn btn-outline btn-primary">Primary</button>
             </div>
             <div className='flex flex-wrap justify-around'>
             {
