e74e54c77815c0ddc2144cc2a71a97fc41399299
Mohd Hannan <mohdhannan7913@gmail.com>
2025-02-15 15:23:06 +0530

    Work on routing

--- e-commerce/src/ProductCard.jsx
+++ e-commerce/src/ProductCard.jsx
@@ -7,6 +7,10 @@ function Card({ items }) {
     const handleNavigation = () => {
       navigate(`/product/${id}`);
     };
+
+    const StopEvent = (e) =>{
+        e.stopPropagation();
+    }
     return (
         <div className="card card-compact bg-white w-96 shadow-xl m-5" onClick={handleNavigation}>
             <figure>
@@ -25,7 +29,7 @@ function Card({ items }) {
                 <p className='text-xl font-bold'>{price} $</p>
                 <p>Rating: {rating}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary text-white">Buy Now</button>
+                    <button className="btn btn-primary text-white" onClick={StopEvent}>Buy Now</button>
                 </div>
             </div>
         </div>
