4b074d7cf0162c40a1215d16bf5df75408bae55d
Mohd Hannan <mohdhannan7913@gmail.com>
2025-02-19 01:07:18 +0530

    crate cart slice for (add remove and clear items )

--- e-commerce/src/utility/store/CartSlice.jsx
+++ e-commerce/src/utility/store/CartSlice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const CartSlice = createSlice({
     name: "Cart",
     initialState:{
-        items: []
+        items: ["Tv","fridge"]
     },
     reducers:{
         addCart:(state, action)=>{
@@ -12,7 +12,17 @@ const CartSlice = createSlice({
             state.items.push(objData)
         },
         removeCart:(state, action)=>{
-            let id = action.payload
+            let id = action.payload;
+            let objIdx = state.items.findIndex((cartObj)=>{cartObj.objData.id === id});
+            state.items.splice(objIdx, 1);
+
+            clearCart: (state, action)=>{
+                state.items.length = 0;
+
+            }
         }
     }
 })
+
+export let {addCart, removeCart, clearCart} = CartSlice.actions;
+export default CartSlice.reducer
