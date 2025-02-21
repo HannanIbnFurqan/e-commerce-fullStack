85fce68cf3c79881489949cca9eca12ab2f5fa6a
Mohd Hannan <mohdhannan7913@gmail.com>
2025-02-21 23:25:17 +0530

    multiple cart add functionality

--- e-commerce/src/Cart.jsx
+++ e-commerce/src/Cart.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import { useSelector } from 'react-redux'
+import CartRow from './CartRow';
 function Cart() {
+  const CartItems = useSelector((store)=> store.cart.items)
+  console.log("cartIems = ",CartItems);
   return (
-    <div>Cart</div>
+    <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Rating</th>
+        <th>Quantity</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    <CartRow></CartRow>
+    </tbody>
+  </table>
+</div>
   )
 }
 
