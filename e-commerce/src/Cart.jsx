d8e81da9203b604e0862e09db029599641cfce12
Mohd Hannan <mohdhannan7913@gmail.com>
2025-02-24 19:15:54 +0530

    built clear cart functionality

--- e-commerce/src/Cart.jsx
+++ e-commerce/src/Cart.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-
+import { useDispatch } from 'react-redux';
+import { clearCart } from './utility/store/CartSlice';
 import CartRow from './CartRow';
 
 function Cart() {
   const CartItems = useSelector((store) => store.cart.items);
   console.log("Cart Items = ", CartItems);
+  const dispatch = useDispatch()
 
   return (
     <div className="overflow-x-auto">
@@ -25,6 +27,9 @@ function Cart() {
           ))}
         </tbody>
       </table>
+      <div>
+        <button  onClick={()=> dispatch(clearCart())}>Clear Cart</button>
+      </div>
     </div>
   );
 }
