d65f453d8905af18ac36f5bfaa46228ed19233a0
Mohd Hannan <mohdhannan7913@gmail.com>
2025-04-20 10:51:31 +0530

    signUp function work very well

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,16 +1,32 @@
 import User from "../Models/userModel.js"
 
-const signUp = (req,res) =>{
-    res.send("This is my first function")
+
+const signUp = async (req, res) => {
+    const { userName, email, password, phoneNumber, address } = req.body
+    try {
+        const existingUser = await User.findOne({ email: email })
+        if (existingUser) {
+            return res.status(403).send({ result: false, message: "User alerdy exist" })
+        }
+
+        const newUser = new User(req.body)
+        const userData = await newUser.save()
+        res.status(200).send({ result: true, message: "User Create Successfully", UserData: userData })
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-const signIn = (req,res) =>{}
+const signIn = (req, res) => {
+    res.send("this is my first function");
+}
 
-const updateUser = (req,res) =>{}
+const updateUser = (req, res) => { }
 
-const getUser = (req,res) =>{}
+const getUser = (req, res) => { }
 
-const logOut = (req,res) =>{}
+const logOut = (req, res) => { }
 
 
-export  {signIn, signUp, updateUser, getUser, logOut}
+export { signIn, signUp, updateUser, getUser, logOut }
