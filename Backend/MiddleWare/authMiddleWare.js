c5999078bc47fc09835bc48d7df8d6578037e2e1
Mohd Hannan <mohdhannan7913@gmail.com>
2025-05-07 00:32:02 +0530

    auth for security

--- Backend/MiddleWare/authMiddleWare.js
+++ Backend/MiddleWare/authMiddleWare.js
@@ -1,7 +1,11 @@
-const auth = (req,res)=>{
+const auth = (req,res, next)=>{
+    let token = req.cookie?.token
    try {
+    if(!token){
+        return res.status(401).send({result: false, message: "user is not authantication"})
+    }
     
    } catch (error) {
-    
+    return res.status(404).send({result: false, message:"server error"})
    }
 }
